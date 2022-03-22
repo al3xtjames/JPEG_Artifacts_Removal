90e0f4f581ab8d0477d100cb96d6b2f8b1e37e2c
Daniel Joseph Sapit <daniel.sapit@eee.upd.edu.ph>
2022-03-22 10:45:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -135,7 +135,8 @@ interface = gr.Interface(
                ["somepanda.jpg",True,100,False,30,8,24],
                ["cemetry.jpg",False,70,False,20,44,77],
                ["michelangelo_david.jpg",True,30,False,12,53,27],
-               ["elon_musk.jpg",False,45,False,15,33,30]],
+               ["elon_musk.jpg",False,45,False,15,33,30],
+               ["text.jpg",True,65,False,24,11,13]],
     title = "JPEG Artifacts Removal [FBCNN]",
     description = "Gradio Demo for JPEG Artifacts Removal. To use it, simply upload your image, "
                   "or click one of the examples to load them. Check out the paper and the original GitHub at the link below. "
